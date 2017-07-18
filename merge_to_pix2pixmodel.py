6d0cd5fd27e4263ad2a17b0a69716c313c196d99
donghaoye <donghaoye@qq.com>
2017-07-18 18:02:14 +0800

    将KTH数据（包含骨架）分成AB两类，然后一一对应，合并到一张图片，存到xxxAB目录

---
+++ merge_to_pix2pixmodel.py
@@ -0,0 +1,66 @@
+# -*- coding: UTF-8 -*-
+import os
+import shutil
+import numpy as np
+import cv2
+
+
+'''
+将real image 和 skeleton image合并到同一张图
+'''
+
+
+def file_copy(src, dst):
+    try:
+        shutil.copy(src, dst)
+    except:
+        error_flag = True
+
+# 将KTH分成A、B两个文件夹
+def split2AB(src='/data/donghaoye/KTH/data/TRAIN', dst='/data/donghaoye/KTH/data/train_A_B'):
+    for fpathe, dirs, fs in os.walk():
+        if os.path.exists(dst) == False:
+            os.makedirs(dst)
+        for f in fs:
+            img_file = os.path.join(fpathe, f)
+            if img_file.find("skeleton") == -1:
+                file_copy(src, dst + "trainA")                  # 拷贝到trainA目录
+            else:
+                file_copy(src, dst + "trainB")                  # 拷贝到trainB目录
+
+
+def combine_A_and_B(fold_A, fold_B, fold_AB):
+    splits = os.listdir(fold_A)
+    for sp in splits:
+        img_fold_A = os.path.join(fold_A, sp)
+        img_fold_B = os.path.join(fold_B, sp)
+        img_list = os.listdir(img_fold_A)
+
+        img_fold_AB = os.path.join(fold_AB, sp)
+        if not os.path.isdir(img_fold_AB):
+            os.makedirs(img_fold_AB)
+
+        for n in range(len(img_list)):
+            name_A = img_list[n]
+            path_A = os.path.join(img_fold_A, name_A)
+            name_B = "ske_" + name_A
+            path_B = os.path.join(img_fold_B, name_B)
+            if os.path.isfile(path_A) and os.path.isfile(path_B):
+                name_AB = name_A
+                path_AB = os.path.join(img_fold_AB, name_AB)
+                im_A = cv2.imread(path_A, cv2.CV_LOAD_IMAGE_COLOR)
+                im_B = cv2.imread(path_B, cv2.CV_LOAD_IMAGE_COLOR)
+                im_AB = np.concatenate([im_A, im_B], 1)
+                cv2.imwrite(path_AB, im_AB)
+
+if __name__=="__main__":
+    src = '/data/donghaoye/KTH/data2/TRAIN'
+    dst = '/data/donghaoye/KTH/data2/train_A_B'
+    fold_A = dst + '/trainA'
+    fold_B = dst + '/trainB'
+    fold_AB = dst + '/trainAB'
+
+    split2AB(src, dst)
+    combine_A_and_B(fold_A, fold_B, fold_AB)
+
+
--- prepare_data.py
+++ prepare_data.py
@@ -4,35 +4,45 @@ import subprocess
 import shutil
 
 
-data_path = '/home/profloo/Documents/KTH'
-# sequences_list = ''
-video_files=os.listdir(data_path + '/data/videos/')
-video_files.sort()
+data_path = '/data/donghaoye/KTH'
+# # sequences_list = ''
+# video_files=os.listdir(data_path + '/data/videos/')
+# video_files.sort()
+#
+# # extract frames from video clips
+# args=['ffmpeg', '-i']
+# for video in video_files:
+# 	print(video)
+# 	video_name = video[:-11]	# remove '_uncomp.avi' from name
+# 	print('video name is: ', video_name)
+# 	frame_name = 'frame_%d.jpg'	# count starts from 1 by default
+# 	os.makedirs(data_path + '/data/frames/'+video_name)
+# 	args.append(data_path + '/data/videos/'+video)
+# 	args.append(data_path + '/data/frames/'+video_name+'/'+frame_name)
+# 	ffmpeg_call = ' '.join(args)
+# 	print(ffmpeg_call)
+# 	print(args)
+# 	subprocess.call(ffmpeg_call, shell=True)		# execute the system call
+# 	args=['ffmpeg', '-i']
+# 	if (video_files.index(video) + 1) % 50 == 0:
+# 		print('Completed till video : ', (video_files.index(video) + 1))
+#
+#
+# print('[MESSAGE]	Frames extracted from all videos')
 
-# extract frames from video clips
-args=['ffmpeg', '-i']
-for video in video_files:
-	# print video
-	video_name = video[:-11]	# remove '_uncomp.avi' from name
-	# print 'video name is: ', video_name
-	frame_name = 'frame_%d.jpg'	# count starts from 1 by default
-	os.makedirs(data_path + '/data/frames/'+video_name)
-	args.append(data_path + '/data/videos/'+video)
-	args.append(data_path + '/data/frames/'+video_name+'/'+frame_name)
-	ffmpeg_call = ' '.join(args)
-	# print ffmpeg_call
-	# print args
-	subprocess.call(ffmpeg_call, shell=True)		# execute the system call
-	args=['ffmpeg', '-i']
-	if (video_files.index(video) + 1) % 50 == 0:
-		print 'Completed till video : ', (video_files.index(video) + 1)
-			
 
-print '[MESSAGE]	Frames extracted from all videos'
-
-os.makedirs(data_path + '/data/' + 'TRAIN')
-os.makedirs(data_path + '/data/' + 'VALIDATION')
-os.makedirs(data_path + '/data/' + 'TEST')
+train_path = data_path + '/data/' + 'TRAIN'
+valid_path = data_path + '/data/' + 'VALIDATION'
+test_path = data_path + '/data/' + 'TEST'
+if os.path.exists(train_path):
+	shutil.rmtree(train_path)
+if os.path.exists(valid_path):
+	shutil.rmtree(valid_path)
+if os.path.exists(test_path):
+	shutil.rmtree(test_path)
+os.makedirs(train_path)
+os.makedirs(valid_path)
+os.makedirs(test_path)
 
 train = [11, 12, 13, 14, 15, 16, 17, 18]
 validation =[19, 20, 21, 23, 24, 25, 1, 4]
@@ -45,47 +55,59 @@ lines = filter(None, lines)
 # split by tabs and remove blank entries
 lines = [filter(None, line.split('\t')) for line in lines]
 lines.sort()
+# print(lines)
 
-success_count=0
-error_count=0
+success_count = 0
+error_count = 0
+seq_path = ''
 for line in lines:
-	vid = line[0].strip(' ')
-	subsequences = line[-1].split(',')
+	vid = line[0].strip(' ').replace(' ', '')
+	if vid.find('jogging') != -1 or vid.find('handclapping') != -1 or vid.find('boxing') != -1:
+		continue
+	subsequences = line[-1].split(',')[1:]
+	print(line)
+	print(vid)
+	print(subsequences)
+
 	person = int(vid[6:8])
 	if person in train:
 		move_to = 'TRAIN'
+		print("Tain!!!!!!!")
 	elif person in validation:
 		move_to = 'VALIDATION'
+		print("V!!!!!!!")
 	else:
 		move_to = 'TEST'
+		print("test!!!!!!!")
 	for seq in subsequences:
 		try:
 			limits=seq.strip(' ').split('-')
 			seq_path=data_path + '/data/' + move_to + '/' + vid + '_frame_' + limits[0] + '_' + limits[1]
 			os.makedirs(seq_path)
 		except:
-			print '-----------------------------------------------------------'
-			print '[ERROR MESSAGE]: '
-			print 'limits : ', limits 
-			print 'seq_path : ', seq_path
-			print '-----------------------------------------------------------'
+			print('-----------------------------------------------------------')
+			print('[ERROR MESSAGE]: ')
+			print('limits : ', limits)
+			print('seq_path : ', seq_path)
+			print('-----------------------------------------------------------')
 			continue
 		error_flag=False
 		for i in xrange(int(limits[0]), int(limits[1])+1):
 			src = data_path + '/data' + '/frames/' + vid + '/frame_' + str(i) + '.jpg'
-			# print i, src, limits
 			dst = seq_path
+			print(dst + "!!!")
+			print(i, src, limits)
 			try:
 				shutil.copy(src, dst)
 			except:
 				error_flag = True
 		if error_flag:
-			print "[ERROR]: ", seq_path
+			print("[ERROR]: ", seq_path)
 			error_count+=1
 
 	if (lines.index(line) + 1) % 50 == 0:
-		print 'Completed till video : ', (lines.index(line) + 1)
+		print('Completed till video : ', (lines.index(line) + 1))
 	success_count+=1
 
-print '[ALERT]		Total error count is : ', error_count
-print '[MESSAGE]	Data split into train, validation and test'
+print('[ALERT]		Total error count is : ', error_count)
+print('[MESSAGE]	Data split into train, validation and test')
