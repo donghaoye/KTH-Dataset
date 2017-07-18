da9b674bee79648bd5a61925ea5895a5db81c4ad
donghaoye <donghaoye@qq.com>
2017-07-18 19:48:03 +0800

    图片文件名加上小目录名称

--- merge_to_pix2pixmodel.py
+++ merge_to_pix2pixmodel.py
@@ -18,40 +18,37 @@ def file_copy(src, dst):
 
 # 将KTH分成A、B两个文件夹
 def split2AB(src, dst):
+    if os.path.exists(dst + "/trainA/") == False:
+        os.makedirs(dst + "/trainA/")
+    if os.path.exists(dst + "/trainB/") == False:
+        os.makedirs(dst + "/trainB/")
+
     for fpathe, dirs, fs in os.walk(src):
-        if os.path.exists(dst) == False:
-            os.makedirs(dst)
         for f in fs:
             img_file = os.path.join(fpathe, f)
             if img_file.find("skeleton") == -1:
-                file_copy(src, dst + "trainA")                  # 拷贝到trainA目录
+                file_copy(img_file, dst + "/trainA/" + img_file.split('/')[-2] + "_" + f)           # 拷贝到trainA目录
             else:
-                file_copy(src, dst + "trainB")                  # 拷贝到trainB目录
+                file_copy(img_file, dst + "/trainB/" + img_file.split('/')[-2] + "_"  + f)          # 拷贝到trainB目录
 
 
 def combine_A_and_B(fold_A, fold_B, fold_AB):
-    splits = os.listdir(fold_A)
-    for sp in splits:
-        img_fold_A = os.path.join(fold_A, sp)
-        img_fold_B = os.path.join(fold_B, sp)
-        img_list = os.listdir(img_fold_A)
+    if not os.path.isdir(fold_AB):
+        os.makedirs(fold_AB)
 
-        img_fold_AB = os.path.join(fold_AB, sp)
-        if not os.path.isdir(img_fold_AB):
-            os.makedirs(img_fold_AB)
-
-        for n in range(len(img_list)):
-            name_A = img_list[n]
-            path_A = os.path.join(img_fold_A, name_A)
-            name_B = "ske_" + name_A
-            path_B = os.path.join(img_fold_B, name_B)
-            if os.path.isfile(path_A) and os.path.isfile(path_B):
-                name_AB = name_A
-                path_AB = os.path.join(img_fold_AB, name_AB)
-                im_A = cv2.imread(path_A, cv2.CV_LOAD_IMAGE_COLOR)
-                im_B = cv2.imread(path_B, cv2.CV_LOAD_IMAGE_COLOR)
-                im_AB = np.concatenate([im_A, im_B], 1)
-                cv2.imwrite(path_AB, im_AB)
+    img_list = os.listdir(fold_A)
+    for n in range(len(img_list)):
+        name_A = img_list[n]
+        path_A = os.path.join(fold_A, name_A)
+        name_B = "ske_" + name_A
+        path_B = os.path.join(fold_B, name_B)
+        if os.path.isfile(path_A) and os.path.isfile(path_B):
+            name_AB = name_A
+            path_AB = os.path.join(fold_AB, name_AB)
+            im_A = cv2.imread(path_A)
+            im_B = cv2.imread(path_B)
+            im_AB = np.concatenate([im_A, im_B], 1)
+            cv2.imwrite(path_AB, im_AB)
 
 if __name__=="__main__":
     src = '/data/donghaoye/KTH/data2/TRAIN'
@@ -61,6 +58,6 @@ if __name__=="__main__":
     fold_AB = dst + '/trainAB'
 
     split2AB(src, dst)
-    combine_A_and_B(fold_A, fold_B, fold_AB)
+    #combine_A_and_B(fold_A, fold_B, fold_AB)
 
 
