4fe2d781be3a5e405fd4457390b412a74e9a9a84
donghaoye <donghaoye@qq.com>
2017-08-15 15:46:33 +0800

    按照pose类型建立的文件夹 waving/walking/running

--- select_topk_by_video_group_by_pose.py
+++ select_topk_by_video_group_by_pose.py
@@ -5,12 +5,12 @@ import shutil
 import numpy as np
 import cv2
 
-
 def get_cur_fold_list(path):
     sub_dir_list = []
     list = os.listdir(path)
     for line in list:
-        filepath = os.path.join(path, line)
+        #filepath = os.path.join(path, line)
+        filepath = path + "/" + line
         if os.path.isdir(filepath):
             if filepath.find("_skeleton") != -1:
                 sub_dir_list.append(filepath)
@@ -43,7 +43,9 @@ def get_dir_img_list(dir_proc, recusive=True):
             if (recusive):
                 file_list.append(get_dir_img_list(os.path.join(dir_proc, file), recusive))
             continue
+        #img = os.path.join(dir_proc, file)
         img = dir_proc + "/" + file
+
         file_list.append(img)
     return file_list
 
@@ -69,6 +71,14 @@ def get_topk_pix_img(img_fold, k):
         real_img_list.append(dict[tk].replace("_skeleton", "").replace("ske_", ""))
     return real_img_list, skeleton_img_list
 
+def write_file_by_list(real_img_list, output_path):
+    for img_path in real_img_list:
+        img = cv2.imread(img_path)
+        output_filename = img_path.split("/")[-2] + "_" + img_path.split("/")[-1]
+        output_file_path = os.path.join(output_path, output_filename)
+        cv2.imwrite(output_file_path, img)
+
+
 def save_topk_img_and_skeleton(input_path, output_path, k):
     output_path_A = os.path.join(output_path, "A")
     output_path_B = os.path.join(output_path, "B")
@@ -77,35 +87,52 @@ def save_topk_img_and_skeleton(input_path, output_path, k):
     if not os.path.exists(output_path_B):
         os.makedirs(output_path_B)
 
+    real_img_list_waving = []
+    real_img_list_walking = []
+    real_img_list_running = []
+    skeleton_img_list_waving = []
+    skeleton_img_list_walking = []
+    skeleton_img_list_running = []
+
     real_img_list, skeleton_img_list = get_topk_pix_img(input_path, k)
 
-    for img_path in real_img_list:
-        img = cv2.imread(img_path)
-        frame_path = img_path.replace("_skeleton", "").replace("ske_", "").replace("TRAIN", "")
-        output_filename = frame_path.split("/")[-2] + "_" + frame_path.split("/")[-1]
-        frame_ref_ske_img_path = output_path_A + "/" + output_filename
-        #cv2.imwrite(frame_ref_ske_img_path, img)
-        print frame_ref_ske_img_path
+    for img in real_img_list:
+        if img.find("waving") != -1:
+            real_img_list_waving.append(img)
+        if img.find("walking") != -1:
+            real_img_list_walking.append(img)
+        if img.find("running") != -1:
+            real_img_list_running.append(img)
 
-    # skeleton
-    for ske_path in skeleton_img_list:
-        ske_img = cv2.imread(ske_path)
-        frame_path = ske_path.replace("_skeleton", "").replace("TRAIN", "")
-        output_filename = frame_path.split("/")[-2] + "_" + frame_path.split("/")[-1]
-        frame_ref_ske_img_path = output_path_B + "/" + output_filename
-        #cv2.imwrite(frame_ref_ske_img_path, ske_img)
+    for img in skeleton_img_list:
+        if img.find("waving") != -1:
+            skeleton_img_list_waving.append(img)
+        if img.find("walking") != -1:
+            skeleton_img_list_walking.append(img)
+        if img.find("running") != -1:
+            skeleton_img_list_running.append(img)
+
+    write_file_by_list(real_img_list_waving, output_path_A)
+    write_file_by_list(real_img_list_walking, output_path_A)
+    write_file_by_list(real_img_list_running, output_path_A)
+    write_file_by_list(skeleton_img_list_waving, output_path_B)
+    write_file_by_list(skeleton_img_list_walking, output_path_B)
+    write_file_by_list(skeleton_img_list_running, output_path_B)
 
 
 
 if __name__=="__main__":
-    # 按照pose类型建立的文件夹
+
+    # 按照pose类型建立的文件夹 waving/walking/running
     # train
-    src = 'I:/KTH/TRAIN'
-    dst = 'I:/KTH/data5/train_A_B/train'
+    # src = 'I:/KTH/TRAIN'
+    # dst = 'I:/KTH/data5/train_A_B/train2'
     # src = /data/donghaoye/KTH/data/VALIDATION
-    # dst = /data/donghaoye/KTH/data5/train_A_B/train
-    #src = sys.argv[1]
-    #dst = sys.argv[2]
+    # dst = /data/donghaoye/KTH/data5/validation_A_B/validation
+    # src = /data/donghaoye/KTH/data/TEST
+    # dst = /data/donghaoye/KTH/data5/test_A_B/test
+    src = sys.argv[1]
+    dst = sys.argv[2]
 
     sub_dir_list = get_cur_fold_list(src)
     for sub_dir in sub_dir_list:
